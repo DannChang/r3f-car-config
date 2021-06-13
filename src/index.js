56e5ceb56af8b07992d126343fdfa18e630a8a7b
dmagma <dmagma@hotmail.com>
2021-06-13 14:24:28 -0700

    Change boilerplate code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Box';
 import reportWebVitals from './reportWebVitals';
+import Box from './Box'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Box />
   </React.StrictMode>,
   document.getElementById('root')
 );
